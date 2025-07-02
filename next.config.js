735d1d61e18f0b956797fa5e71b48d8644cecaa1
acostahenry7 <73194577+acostahenry7@users.noreply.github.com>
2025-07-02 18:42:34 -0400

    test-gh-actions

--- next.config.js
+++ next.config.js
@@ -9,11 +9,12 @@ const nextConfig = {
   },
 };
 
-module.exports = withSentryConfig(nextConfig, {
-  org: "kisoft-21",
-  project: "76692f07c07b42b598b8a44f3d85d99d",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+module.exports = nextConfig;
+// withSentryConfig(nextConfig, {
+//   org: "kisoft-21",
+//   project: "76692f07c07b42b598b8a44f3d85d99d",
+//   silent: !process.env.CI,
+//   widenClientFileUpload: true,
+//   disableLogger: true,
+//   automaticVercelMonitors: true,
+// });
